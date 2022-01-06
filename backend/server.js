84a277d456cb5a1849242e2810768b1e4b038cf4
G Chao <81912588+powerlesbian@users.noreply.github.com>
2022-01-06 13:58:11 +0800

    refactored files

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5050;
+const helmet = require("helmet");
+const { join } = require("path");
 
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(express.static(join(__dirname, "build")));
 
 // get driver connection
 const dbo = require("./db/conn");
@@ -19,3 +25,8 @@ app.listen(port, () => {
   });
   console.log(`Server is running on port: ${port}`);
 });
+
+//////////below from Auth0 server.js file:
+
+// const port = process.env.SERVER_PORT || 3000;
+// app.listen(port, () => console.log(`Server listening on port ${port}`));

d3b9d26ec104d76f2d45c195cda72a1dfa7bcc2d
G Chao <81912588+powerlesbian@users.noreply.github.com>
2022-01-08 10:13:39 +0800

    forget about docker

---
+++ frontend/build/precache-manifest.dfd8da7a6c4de37f924a43e3b7b42199.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5de0ccf49145a7a5dbe8af728c7052",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fc9bc9a8f99e62631d9a",
+    "url": "./static/css/2.11b452ed.chunk.css"
+  },
+  {
+    "revision": "8ba946a7e1c9df5ff117",
+    "url": "./static/css/main.51d06244.chunk.css"
+  },
+  {
+    "revision": "fc9bc9a8f99e62631d9a",
+    "url": "./static/js/2.2d103b0b.chunk.js"
+  },
+  {
+    "revision": "0033d88a2ef0997ebce90193519c3303",
+    "url": "./static/js/2.2d103b0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ba946a7e1c9df5ff117",
+    "url": "./static/js/main.2c9eaf58.chunk.js"
+  },
+  {
+    "revision": "ad107e3f8ca158dac0f8",
+    "url": "./static/js/runtime-main.8782c25d.js"
+  },
+  {
+    "revision": "504ae16e567fe21104d5333a3d34e4e5",
+    "url": "./static/media/7711009.504ae16e.png"
+  },
+  {
+    "revision": "dfd1a262809590cdcac1cc95aa43cd91",
+    "url": "./static/media/chineseceramic.dfd1a262.jpg"
+  },
+  {
+    "revision": "14ce74cea14c127b5ab7f3a939a74488",
+    "url": "./static/media/creepy.14ce74ce.jpg"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "./static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "5276972b80ec2fc420d227a32d52f837",
+    "url": "./static/media/logo.5276972b.svg"
+  },
+  {
+    "revision": "b849e98142e77239f7b5fa05a1a48883",
+    "url": "./static/media/macaron.b849e981.JPG"
+  },
+  {
+    "revision": "97ae55d939ed151eda1a909baa752074",
+    "url": "./static/media/piggyWhiskyLand.97ae55d9.jpg"
+  }
+]);
